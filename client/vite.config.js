ee51fadb4a2d18c3d4d62d35c0ceb9608d54a42f
Olga <yurkovaos@gmail.com>
2024-10-16 14:43:17 +0700

    screens added

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         game: resolve(__dirname, "pages/game.html"),
         gamelib: resolve(__dirname, "pages/gamelib.html"),
-        games: resolve(__dirname, "assets/js/games.js"),
+        games: resolve(__dirname, "assets/js/game.js"),
         gameslibjs: resolve(__dirname, "assets/js/gamelib.js"),
       },
     },
