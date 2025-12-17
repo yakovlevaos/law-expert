6882811b4f857d719d68e527205d351c39e8de08
Olga <yurkovaos@gmail.com>
2025-12-17 22:37:58 +0700

    added small changes to the project my mirror

---
+++ client/dist/assets/main-4d3KDDs5.js
@@ -0,0 +1,85 @@
+import"./modulepreload-polyfill-9p4a8sJU.js";import"./index-Aslqewt8.js";import{i as d}from"./modal-lZnzjzJK.js";/* empty css              */import{S as p}from"./experts-ErTPk-7q.js";const m=[{index:1,title:"Мои отражения",period:"Июль – ноябрь 2025 года",coverage:"100 подростков",place:"г. Красноярск",funding:"Субсидия в рамках конкурса «Ты-город», собственные средства.",partners:["Главное управление образования администрации г. Красноярск.","Муниципальное автономное учреждение «Центр психолого-педагогической, медицинской и социальной помощи «Эго».","Муниципальное молодежное автономное учреждение «Молодежный центр «База».","Благотворительный фонд «Счастливые дети».","Региональная общественная организация «Совет отцов».","Муниципальное автономное учреждение «Дом Кино»"],description:"Проект посвящен творческому самовыражению подростков через фотографию. С начала августа до конца октября подростки посещали Молодёжный центр «БАЗА», где с ними работали профессиональный фотограф и психолог. Фотограф провел обучение базовым навыкам фотографирования и постобработки в виде лекционных и практических занятий. Психолог провел лекционные занятия по темам эмоций, восприятия, мотивации и практические занятия совместно с фотографом, направленные на работу с эмоциями, раскрепощением, повышением уверенности в себе и мотивации. Большим, завершающим мероприятием стала фотовыставка лучших работ и торжественное награждение участников. Мероприятие состоялось 2 ноября 2025 с 12:00 до 18:00 в пространстве кинотеатра «Дом Кино».",images:["mirror1","mirror2","mirror3","mirror4","mirror5.mp4","mirror6.mp4"]},{index:2,title:"Цифровой патриотизм: современное патриотическое воспитание с применением игровых технологий",period:"Март – ноябрь 2025 года",coverage:"Не менее 500 детей.",place:"г. Красноярск",funding:"Субсидия департамента социального развития администрации города Красноярска.",partners:["Муниципальное автономное учреждение «Центр психолого-педагогической, медицинской и социальной помощи «Эго»","Загородный стационарный детский оздоровительный лагерь «Ласточка»","Муниципальное молодежное автономное учреждение «ИТ-Центр»","Муниципальное молодежное автономное учреждение «Молодежный военно-спортивный центр «Патриот»"],description:"Проект направлен на внедрение цифровых-игровых технологий в современное патриотическое воспитание с применением инновационных психологических методик. Игровой формат позволяет не только привлекать внимание молодежной аудитории, но и эффективно передавать знания об истории, культуре и ценностях своей страны. Видеоигры с элементами исторической реконструкции, тактического планирования и командной работы могут стать отличной основой для патриотического воспитания, создавая у игроков чувство принадлежности и ответственности.",images:["cyberpatriot1","cyberpatriot2","cyberpatriot3","cyberpatriot4","cyberpatriot5","cyberpatriot6","cyberpatriot7"]},{index:3,title:"Психологические консультации с помощью видеоигр",period:"Летний период 2024 года.",coverage:"Более 1500 детей, проживающих на территории города Красноярска.",place:"Загородный лагерь «Ласточка».",funding:"Cубсидия департамента социального развития администрации города Красноярска, собственные средства.",partners:["Муниципальное автономное учреждение «Центр психолого-педагогической, медицинской и социальной помощи «Эго»","Загородный стационарный детский оздоровительный лагерь «Ласточка»","Муниципальное молодежное автономное учреждение «ИТ-Центр»"],description:`Дети-подростки в возрасте с 10 до 17 лет, отдыхающие в
+      муниципальном загородном оздоровительном лагере «Ласточка», на
+      протяжении 4-ех сезонов проходили индивидуальные
+      психологические консультации с психологами «Генезис» с помощью
+      новейшей авторской методики, которая в своей работы использует
+      видеоигры в качестве инструмента. Кроме того, проведены
+      семинары о видеоиграх и их месте в жизни, культуре и их
+      влиянии на психологическое и эмоциональное здоровье.`,images:["gameplayproject1","gameplayproject2","gameplayproject3","gameplayproject4"]},{index:4,title:"Киберспортивная психология",period:"Июнь-Сентябрь 2024 года.",coverage:"32 подростка в возрасте 14-18 лет, проживающих в городе Красноярске.",place:"Муниципальное молодежное автономное учреждение «ИТ-Центр».",funding:"Субсидия в рамках конкурса «Ты-город», собственные средства.",partners:["Главное управление по физической культуре и спорту администрации города Красноярска","Главное управление образования администрации города Красноярска","Муниципальное автономное учреждение «Центр психолого-педагогической, медицинской и социальной помощи «Эго»","Муниципальное молодежное автономное учреждение «ИТ-Центр»","Телеканал «Енисей»"],description:"В рамках проекта на протяжении летнего периода 2024 года проводились на еженедельной основе киберспортивные тренировки по дисциплинам: Dota 2, Counter-strike 2, MK1 и EA FC 24 с участием киберспортивного тренера и психолога. Прорабатывались: умение работать с собственными эмоциями, повышение стрессоустойчивости, развитие коммуникативных навыков, повышение уверенности в себе. Кроме того, еженедельно проводились ценностно-смысловые дни, в рамках которых подростки совместно с психологами проходили сюжетноориентированные игры (The last of us, Ghost of Tsushima, God of war: Ragnarek), где прорабатывались: самоосознание, семейные ценности, инициатива и т.д. По итогам проекта был проведен киберспортивный турнир, а также проведена работа с родителями, в результате которой был снят видеоподкаст с психологами, где были разобраны все вопросы родительского сообщества о видеоиграх и их влиянии на детей. Публикации в СМИ: https://www.enisey.tv/news/post-69581/",images:["cypepsycho1","cypepsycho2","cypepsycho3","cypepsycho4","cypepsycho5","cypepsycho6","cypepsycho7"]}],g={init(){document.querySelectorAll(".card").forEach(e=>{const t=e.querySelector(".tabs-menu");Array.from(t.children).forEach(s=>{s.addEventListener("click",()=>{this.toggle(e,s.dataset.target)}),s.classList.contains("is-active")&&this.toggle(e,s.dataset.target)})})},toggle(e,t){e.querySelectorAll(".tab-content").forEach(i=>{i.style.display=i.id===t?"block":"none"}),e.querySelectorAll(".tabs-menu li").forEach(i=>{const r=i.querySelector("a"),o=i.dataset.target===t;i.classList.toggle("is-active",o),r.style.color=o?"blue":"grey"}),t.startsWith("tab-")&&t.endsWith("3")&&this.initSwiper(e)},initSwiper(e){const t=e.querySelector(".swiper-container");t&&!t.swiper&&new p(t,{slidesPerView:1,centeredSlides:!0,spaceBetween:10,loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}})}},u=(e,t)=>{const a=t%2===0?"dark:bg-slate-900 bg-slate-100 md:rounded-2xl md:w-4/6 md:m-4 p-4":"dark:bg-slate-900 bg-slate-100 md:rounded-2xl md:w-4/6 md:m-4 p-4 md:ml-auto",s=e.images[0],[i,r]=s.split("."),o=r&&r.toLowerCase()==="mp4"?`/images/${s}`:r?`/images/${s}`:`/images/${i}.jpeg`;return`
+    <div class="card ${a}">
+      <div class="tabs">
+        <ul class="tabs-menu">
+          <li class="is-active" data-target="tab-${t}-1">
+            <a>Проект</a>
+          </li>
+          <li data-target="tab-${t}-2">
+            <a>Описание проекта</a>
+          </li>
+          <li data-target="tab-${t}-3">
+            <a>Фото</a>
+          </li>
+        </ul>
+      </div>
+      <div class="tab-content" id="tab-${t}-1" style="display:none;">
+        <h2 class="text-center">${e.title}</h2>
+        <table class="table border-separate border-spacing-y-2 dark:bg-slate-900 bg-slate-100 dark:text-slate-100 text-slate-900 w-full">
+          <tbody>
+            <tr>
+              <td class="max-sm:hidden w-1/3 pr-2 mb-0" rowspan="4">
+                ${r&&r.toLowerCase()==="mp4"?`<video src="${o}" alt="Media ${t}" class="w-full cursor-pointer card-screenshot" controls />`:`<img src="${o}" alt="Image ${t}" class="w-full cursor-pointer card-screenshot" />`}
+              </td>
+              <td class="font-bold">Срок реализации:</td>
+              <td>${e.period}</td>
+            </tr>
+            <tr>
+              <td class="font-bold">Охват:</td>
+              <td>${e.coverage}</td>
+            </tr>
+            <tr>
+              <td class="font-bold">Место проведения:</td>
+              <td>${e.place}</td>
+            </tr>
+            <tr>
+              <td class="font-bold">Источник финансирования:</td>
+              <td>${e.funding}</td>
+            </tr>
+            <tr>
+              <td class="font-bold">Партнеры:</td>
+              <td colspan="2">${e.partners.join("<br />")}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="tab-content px-2" id="tab-${t}-2" style="display:none;">
+        <div class="flex">
+          ${r&&r.toLowerCase()==="mp4"?`<video
+                src="${o}"
+                alt="Видеоигры1"
+                class="max-sm:hidden w-1/3 h-full cursor-pointer pr-4 card-screenshot"
+                controls
+              />`:`<img
+                src="${o}"
+                alt="Видеоигры1"
+                class="max-sm:hidden w-1/3 h-full cursor-pointer pr-4 card-screenshot"
+              />`}
+          <p>${e.description}</p>
+        </div>
+      </div>
+      <div class="tab-content" id="tab-${t}-3" style="display:none;">
+        <div class="swiper-container">
+          <div class="swiper-wrapper">
+            ${e.images.map(n=>{const[l,c]=n.split(".");return c&&c.toLowerCase()==="mp4"?`<div class="swiper-slide flex justify-center items-center">
+                      <video src="/images/${n}" alt="Video Slide" class="object-contain w-full cursor-pointer card-screenshot" controls />
+                    </div>`:`<div class="swiper-slide flex justify-center items-center">
+                      <img src="${c?`/images/${n}`:`/images/${l}.jpeg`}" alt="Image Slide" class="object-contain w-full cursor-pointer card-screenshot" />
+                    </div>`}).join("")}
+          </div>
+          <!-- Pagination -->
+          <div class="swiper-pagination"></div>
+          <!-- Navigation buttons -->
+          <div class="swiper-button-prev swiper-navBtn"></div>
+          <div class="swiper-button-next swiper-navBtn"></div>
+        </div>
+      </div>
+    </div>
+  `},b=()=>{const e=document.getElementById("cardsContainer");e.innerHTML=m.map((a,s)=>u(a,s)).join(""),g.init();const t=d();t&&document.querySelectorAll(".card-screenshot").forEach(a=>{a.addEventListener("click",function(){t(this.src)})})};b();
